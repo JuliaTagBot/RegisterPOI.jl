f20361a33b5d393ee5b09fd874422bf30812c45f
yakir12 <12.yakir@gmail.com>
2019-04-16 11:14:50 +0200

    added defaults

--- src/RegisterPOI.jl
+++ src/RegisterPOI.jl
@@ -30,6 +30,17 @@ function goodtime(x)
     end
 end
 
+function setgetdefault(i::Int, video_menu::RadioMenu)
+    if i == 1
+        i = findlast(isequal(video_menu.options[1]), video_menu.options)
+        i = i - 1
+    else
+        i = i - 1
+        video_menu.options[1] = videofile[i].file_name
+    end
+    return i
+end
+
 
 done_file = joinpath(homedir(), "RegisterPOI.csv")
 if !isfile(done_file)
@@ -39,7 +50,13 @@ if !isfile(done_file)
 end
 
 videofile = load(joinpath(@__DIR__, "videofile.jdb"))
-video_menu = RadioMenu(string.(select(videofile, :file_name)))
+video_menu = RadioMenu(["---"; string.(select(videofile, :file_name))])
+
+mutable struct TimeContainer
+    x::Nanosecond
+end
+lasttime = TimeContainer(Nanosecond(0))
+
 
 function main()
     x0 = CSV.File(done_file)
@@ -64,21 +81,32 @@ function main()
                 printstyled("POI: ", bold = true, color = :yellow)
                 println(poi.type)
                 i = request("In which video file did this POI start?", video_menu)
+                i = setgetdefault(i, video_menu)
+                if i == 0
+                    printstyled("No default set yet. Try again…\n", bold = true, color = :red)
+                    @goto start_poi
+                end
                 start_video = videofile[i]
                 @label start_time
-                println("When in this video file did this POI start (ss.xxx, mm:ss.xxx, or hh:mm:ss.xxx)?")
-                _start = goodtime(strip(readline()))
-                if _start ≡ nothing
-                    printstyled("Malformed time. Try again…\n", bold = true, color = :red)
-                    @goto start_time
+                println("When in this video file did this POI start (press Enter for ", Time(0) + lasttime.x, ")?" )
+                __start = strip(readline())
+                if !isempty(__start)
+                    tmp = goodtime(__start)
+                    if tmp ≡ nothing
+                        printstyled("Malformed time. Try again…\n", bold = true, color = :red)
+                        @goto start_time
+                    end
+                    if tmp > start_video.duration
+                        printstyled("Starting time is longer than the duration of this video file (~$(round(Int, tosecond(start_video.duration))) seconds). Try again…\n", bold = true, color = :red)
+                        @goto start_time
+                    end
+                    lasttime.x = tmp
                 end
-                if _start > start_video.duration
-                    printstyled("Starting time is longer than the duration of this video file (~$(round(Int, tosecond(start_video.duration))) seconds). Try again…\n", bold = true, color = :red)
-                    @goto start_time
-                end
+                _start = lasttime.x
                 start = reduce(+, [r.duration for r in videofile if r.video == start_video.video && r.index < start_video.index], init = _start)
                 # println("Start time specified: ", Time(0) + start)
                 i = request("In which video file did this POI stop?", video_menu)
+                i = setgetdefault(i, video_menu)
                 stop_video = videofile[i]
                 if start_video.video ≠ stop_video.video
                     g1 = filter(isequal(start_video.video), videofile, select = :video)
@@ -93,16 +121,21 @@ function main()
                     @goto start_poi
                 end
                 @label stop_time
-                println("When in this video did this POI stop (ss.xxx, mm:ss.xxx, or hh:mm:ss.xxx)?")
-                _stop = goodtime(strip(readline()))
-                if _stop ≡ nothing
-                    printstyled("Malformed time. Try again…\n", bold = true, color = :red)
-                    @goto stop_time
+                println("When in this video file did this POI stop (press Enter for ", Time(0) + lasttime.x, ")?" )
+                __stop = strip(readline())
+                if !isempty(__stop)
+                    tmp = goodtime(__stop)
+                    if tmp ≡ nothing
+                        printstyled("Malformed time. Try again…\n", bold = true, color = :red)
+                        @goto stop_time
+                    end
+                    if tmp > stop_video.duration
+                        printstyled("stoping time is longer than the duration of this video file (~$(round(Int, tosecond(stop_video.duration))) seconds). Try again…\n", bold = true, color = :red)
+                        @goto stop_time
+                    end
+                    lasttime.x = tmp
                 end
-                if _stop > stop_video.duration
-                    printstyled("Stoping time is longer than the duration of this video file (~$(round(Int, tosecond(stop_video.duration))) seconds). Try again…\n", bold = true, color = :red)
-                    @goto stop_time
-                end
+                _stop = lasttime.x
                 stop = reduce(+, [r.duration for r in videofile if r.video == stop_video.video && r.index < stop_video.index], init = _stop)
                 if start > stop
                     printstyled("Stoping time comes after starting time. Try again…\n", bold = true, color = :red)
